52df946fdaef03d28a23e529824dcc35d7a2bb1a
lydericlandry <lydericlandry@penguin.lyderic.net>
2020-04-24 14:57:04 +0100

    setup of GOBIN variable to avoid any clash

--- deploy-and-run.sh
+++ deploy-and-run.sh
@@ -7,7 +7,7 @@ main() {
 		usage
 	fi
 	utility=${HOME}/go/bin/linux_arm/rpiinfo
-	GOOS=linux GOARCH=arm go install
+	GOBIN=$HOME/go/bin GOOS=linux GOARCH=arm go install
 	if [ $? -eq 0 ] ; then
 		scp ${utility} ${RASPBERRY_PI}:bin
 		ssh ${RASPBERRY_PI} /home/pi/bin/rpiinfo
